67742f71de3de0184f3e4f55859840b4bc2c02b7
Leandro Teixeira <leandro7teixeita@gmail.com>
2025-12-13 14:00:16 -0300

    feat: adicionando o crud para categoria e produto

---
+++ app/crud/categoria.py
@@ -0,0 +1,23 @@
+from sqlalchemy.orm import Session
+from app.models.categoria import Categoria
+from app.schemas.categoria import CategoriaCreate
+
+def criar_categoria(db: Session, dados: CategoriaCreate):
+    categoria = Categoria(nome=dados.nome)
+    db.add(categoria)
+    db.commit()
+    db.refresh(categoria)
+    return categoria
+
+
+def listar_categorias(db: Session):
+    return db.query(Categoria).all()
+
+
+def buscar_categoria(db: Session, categoria_id: int):
+    return db.query(Categoria).filter(Categoria.id == categoria_id).first()
+
+
+def deletar_categoria(db: Session, categoria: Categoria):
+    db.delete(categoria)
+    db.commit()
---
+++ app/crud/produto.py
@@ -0,0 +1,35 @@
+from sqlalchemy.orm import Session
+from app.models.produto import Produto
+from app.schemas.produto import ProdutoCreate, ProdutoPrecoUpdate
+
+
+def criar_produto(db: Session, dados: ProdutoCreate):
+    produto = Produto(
+        nome=dados.nome,
+        preco=dados.preco,
+        categoria_id=dados.categoria_id
+    )
+    db.add(produto)
+    db.commit()
+    db.refresh(produto)
+    return produto
+
+
+def listar_produtos(db: Session):
+    return db.query(Produto).all()
+
+
+def buscar_produto(db: Session, produto_id: int):
+    return db.query(Produto).filter(Produto.id == produto_id).first()
+
+
+def atualizar_preco_produto(db: Session, produto: Produto, dados: ProdutoPrecoUpdate):
+    produto.preco = dados.preco
+    db.commit()
+    db.refresh(produto)
+    return produto
+
+
+def deletar_produto(db: Session, produto: Produto):
+    db.delete(produto)
+    db.commit()
