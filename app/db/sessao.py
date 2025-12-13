21070914b0f7ae128bdb36969f955d26ab6e88a0
Leandro Teixeira <leandro7teixeita@gmail.com>
2025-12-13 02:06:53 -0300

    feat: adicionando as classes de categoria e fazendo o bd

---
+++ app/db/sessao.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.core.config import database_url
+
+engine = create_engine(database_url, connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
